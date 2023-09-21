dc042cedf83fcee99e593997a3707b68a90ff92c
Rizky Al Arief <rizkyalarief11@gmail.com>
2023-09-21 15:28:00 +0700

    Tugas Maxy DAY 5

--- Day4_laravel-boilerplate_Rizky Al Arief/storage/framework/views/275cc6ddc0ed4e6f53ff6fbbc048b2efeb4fe69b.php
+++
@@ -1,6 +0,0 @@
-<div class="pull-right">
-    &copy; <?php echo e(date('Y')); ?> <?php echo e(config('app.name')); ?>. <?php echo e(__('views.backend.section.footer.copyright')); ?>
-
-</div>
-<div class="clearfix"></div>
-<?php /**PATH C:\Users\rizky\OneDrive\Documents\0 UPN INFORMATIKA\temp\New folder\Day4_laravel-boilerplate_Rizky Al Arief\resources\views/admin/sections/footer.blade.php ENDPATH**/ ?>
--- Day4_laravel-boilerplate_Rizky Al Arief/storage/framework/views/f1b58c957821aa5eaa64a7e62b91a44e38e8650f.php
+++
@@ -1,6 +0,0 @@
-<div class="pull-right">
-    &copy; <?php echo e(date('Y')); ?> <?php echo e(config('app.name')); ?>. <?php echo e(__('views.backend.section.footer.copyright')); ?>
-
-</div>
-<div class="clearfix"></div>
-<?php /**PATH C:\Users\rizky\OneDrive\Documents\0 UPN INFORMATIKA\Maxy academy\day 3\laravel-boilerplate\resources\views/admin/sections/footer.blade.php ENDPATH**/ ?>
